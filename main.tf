5f43216a46435a7ab9ee4e3624afe880070e69e1
Kishore Kumar <kkvp1984@gmail.com>
2023-10-07 07:16:38 +0530

    TF S3 bucket creation and documentation updated (#14)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
@@ -11,9 +15,20 @@ provider "random" {
   # Configuration options
 }
 
+resource "aws_s3_bucket" "example" {
+  #Bucket naming rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.id
+
+}
+
 resource "random_string" "bucket_name" {
+  #random TF provider
+  #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
   length           = 27
   special          = false
+  lower            = true
+  upper            = false
 }
 
 output "random_bucket_name"{
